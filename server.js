6cc0ead02e9cb6c023328305db6f57251b276259
magdielperez2005 <abdimelendez14@gmail.com>
2025-05-05 19:18:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post("/comprar", async (req, res) => {
     }
 });
 
-// Puerto dinámico para Render
+// Puerto dinámico para Render o local
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
